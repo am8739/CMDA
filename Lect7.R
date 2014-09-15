e8aa2db06aff02c1c812d24846d11d49f0446f52
Amanda Marousis <am8739@vt.edu>
2014-09-15 11:09:07 -0400

    Revert "Projects/Homeworks"
    This reverts commit c93790813afc2346dd2ee37c65705f845cddfec3.

--- Lect7.R
+++ Lect7.R
@@ -17,11 +17,7 @@ summary(custdata) #for the entire data frame
 #look at individual variables to spot problems
 
 summary(custdata$is.employed)
-#
 summary(custdata$income)
-#There are 328 missing values, which could point to an error because it is extreme
-#A minimum income of zero might not be useful in the data
 summary(custdata$age)
-#The minimum is fairly useless to our analysis.
-#The Maximum is an outlier that seems to be an entry error so we should delete it.
+
 
